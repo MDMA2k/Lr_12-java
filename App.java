5109836f4bd879a32902005403b31d3ccffc1fe0
Igor <igorloev7@gmail.com>
2024-03-26 12:46:28 +0300

    add

---
+++ App.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class App {
+    public App() {
+    }
+
+    public static void main(String[] args) {
+        String name = "Igor";
+        System.out.println("Hello, " + name);
+    }
+}
